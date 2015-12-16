2a45f50c10a0d506f11feed6f5c46881b945cee8
Thiago Tadashi <tadasshi@gmail.com>
2015-12-16 14:36:04 -0200

    commit inicial

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def contact
+  end
+
+  def about_us
+  end
+end
---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get about_us" do
+    get :about_us
+    assert_response :success
+  end
+
+end
