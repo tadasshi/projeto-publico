eaf527b3b976aa92a6710fa6b75333262dcbc2be
Thiago Tadashi <tadasshi@gmail.com>
2016-03-11 16:28:05 -0300

    - linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -85,6 +85,7 @@ set :passenger_restart_with_touch, true
 ## Linked Files & Directories (Default None):
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, fetch(:linked_dirs, []).push('public/uploads')
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
