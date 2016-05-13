93cada935313cdf2e8241bd9cf760b3494e603ab
Thiago Tadashi <tadasshi@gmail.com>
2016-05-13 17:14:52 -0300

    share buttons, error, json

--- app/controllers/metaphors_controller.rb
+++ app/controllers/metaphors_controller.rb
@@ -107,9 +107,9 @@ class MetaphorsController < ApplicationController
 
   # private
   # Use callbacks to share common setup or constraints between actions.
-  # def set_metaphor
-  #   @metaphor = Metaphor.find(params[:id])
-  # end
+   def set_metaphor
+     @metaphor = Metaphor.find(params[:id])
+   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   # def metaphor_params
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -107,9 +107,9 @@ class PostsController < ApplicationController
 
   # private
     # Use callbacks to share common setup or constraints between actions.
-    # def set_post
-    #   @post = Post.find(params[:id])
-    # end
+     def set_post
+       @post = Post.find(params[:id])
+     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     # def post_params
